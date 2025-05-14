87681351a3ed4c2892aa3e6321a5834eb81feb6d
smnnmn <leejs899797@gmail.com>
2025-05-14 19:43:56 +0900

    {2025.5.14} 도로 관리자 시스템

--- Game2/Assets/Scripts/RoadManager.cs
+++ Game2/Assets/Scripts/RoadManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RoadManager : MonoBehaviour
 {
+    [SerializeField] float speed;
     [SerializeField] List<GameObject> roads = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,9 @@ public class RoadManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < roads.Count; i++)
+        {
+            roads[i].transform.Translate(speed * Vector3.back * Time.deltaTime);
+        }
     }
 }
